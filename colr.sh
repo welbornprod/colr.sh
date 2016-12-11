3532304a73d48eabda88c9d3442d4518a59f0b55
Christopher Welborn <cj@welbornprod.com>
2016-12-10 22:25:32 -0600

    Initial commit, v.0.1.1.

---
+++ colr.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+
+# Bash color function to colorize text by name, instead of number.
+# Also includes maps from name to escape code for fore, back, and styles.
+# -Christopher Welborn 08-27-2015
+
+# Variables are namespaced to not interfere when sourced.
+colr_app_name="Colr"
+colr_app_version="0.1.1"
+colr_app_path="$(readlink -f "${BASH_SOURCE[0]}")"
+colr_app_script="${colr_app_path##*/}"
+
+# This flag can be set with colr_enable or colr_disable.
+colr_disabled=0
+
+# Functions to format a color number into an actual escape code.
+function codeformat {
+    # Basic fore, back, and styles.
+    printf "\033[%sm" "$1"
+}
+function extforeformat {
+    # 256 fore color
+    printf "\033[38;5;%sm" "$1"
+}
+function extbackformat {
+    # 256 back color
+    printf "\033[48;5;%sm" "$1"
+}
+
+# Maps from color/style name -> escape code.
+declare -A fore back style
+
+function build_maps {
+    # Build the fore/back maps.
+    # Names and corresponding base code number
+    local colornum
+    # shellcheck disable=SC2102
+    declare -A colornum=(
+        [black]=0
+        [red]=1
+        [green]=2
+        [yellow]=3
+        [blue]=4
+        [magenta]=5
+        [cyan]=6
+        [white]=7
+        )
+    local cname
+    for cname in "${!colornum[@]}"; do
+        fore[$cname]="$(codeformat $((30 + ${colornum[$cname]})))"
+        fore[light$cname]="$(codeformat $((90 + ${colornum[$cname]})))"
+        back[$cname]="$(codeformat $((40 + ${colornum[$cname]})))"
+        back[light$cname]="$(codeformat $((100 + ${colornum[$cname]})))"
+    done
+    # shellcheck disable=SC2154
+    fore[reset]="$(codeformat 39)"
+    back[reset]="$(codeformat 49)"
+
+    # 256 colors.
+    local cnum
+    for cnum in {0..255}; do
+        fore[$cnum]="$(extforeformat "$cnum")"
+        back[$cnum]="$(extbackformat "$cnum")"
+    done
+
+    # Map of base code -> style name
+    local stylenum
+    # shellcheck disable=SC2102
+    declare -A stylenum=(
+        [reset]=0
+        [bright]=1
+        [dim]=2
+        [italic]=3
+        [underline]=4
+        [flash]=5
+        [highlight]=7
+        [normal]=22
+    )
+    local sname
+    for sname in "${!stylenum[@]}"; do
+        style[$sname]="$(codeformat "${stylenum[$sname]}")"
+    done
+}
+build_maps
+
+function colr {
+    # Colorize a string.
+    local text="$1"
+    if ((colr_disabled)); then
+        # Color has been globally disabled.
+        echo -en "$text"
+        return
+    fi
+
+    local forecolr="${2:-reset}"
+    local backcolr="${3:-reset}"
+    local stylename="${4:-normal}"
+
+    declare -a codes resetcodes
+    if [[ "$stylename" =~ ^reset ]]; then
+        resetcodes=("${style[$stylename]}" "${resetcodes[@]}")
+    else
+        codes=("${codes[@]}" "${style[$stylename]}")
+    fi
+
+    if [[ "$backcolr" =~ reset ]]; then
+        resetcodes=("${back[$backcolr]}" "${resetcodes[@]}")
+    else
+        codes=("${codes[@]}" "${back[$backcolr]}")
+    fi
+
+    if [[ "$forecolr" =~ reset ]]; then
+        resetcodes=("${fore[$forecolr]}" "${resetcodes[@]}")
+    else
+        codes=("${codes[@]}" "${fore[$forecolr]}")
+    fi
+
+    # Reset codes must come first (style reset can affect colors)
+    local rc
+    for rc in "${resetcodes[@]}"; do
+        echo -en "$rc"
+    done
+    local c
+    for c in "${codes[@]}"; do
+        echo -en "$c"
+    done
+    local closing="\033[m"
+
+    echo -n "$text"
+    echo -en "$closing"
+}
+
+function colr_auto_disable {
+    # Auto disable colors if stdout is not a tty,
+    # or if the user supplied file descriptors are not ttys.
+    # Arguments:
+    #  $@ : One or more TTY numbers to check.
+    #       Default: 1
+
+    if (($# == 0)); then
+        # Just check stdout by default.
+        if [[ ! -t 1 ]] || [[ -p 1 ]]; then
+            colr_disabled=1
+        fi
+        return
+    fi
+    # Make sure all user's tty args are ttys.
+    local ttynum
+    for ttynum in "$@"; do
+        if [[ ! -t "$ttynum" ]] || [[ -p "$ttynum" ]]; then
+            colr_disabled=1
+            break
+        fi
+    done
+}
+
+function colr_enable {
+    # Re-enable colors after colr_disable has been called.
+    colr_disabled=0
+}
+
+function colr_disable {
+    # Disable colors for the `colr` function.
+    colr_disabled=1
+}
+
+function print_usage {
+    # Show usage reason if first arg is available.
+    [[ -n "$1" ]] && echo -e "\n$1\n"
+
+    echo "${fore[blue]}${style[bright]}$colr_app_name v. $colr_app_version${style[reset]}
+
+    Usage:${fore[magenta]}
+        $colr_app_script -h | -v
+        $colr_app_script TEXT FORE [BACK] [STYLE]
+    ${style[reset]}
+    Options:${fore[green]}
+        BACK          : Name of back color for the text.
+        FORE          : Name of fore color for the text.
+        STYLE         : Name of style for the text.
+        TEXT          : Text to colorize.
+        -h,--help     : Show this message.
+        -v,--version  : Show $colr_app_name version and exit.
+    ${style[reset]}
+    "
+}
+
+
+export colr
+export fore
+export back
+export style
+
+if [[ "$0" == "$BASH_SOURCE" ]]; then
+    declare -a userargs
+    do_forced=0
+    for arg; do
+        case "$arg" in
+            "-f"|"--force" )
+                do_forced=1
+                ;;
+            "-h"|"--help" )
+                print_usage ""
+                exit 0
+                ;;
+            "-v"|"--version" )
+                echo -e "$colr_app_name v. $colr_app_version\n"
+                exit 0
+                ;;
+            -*)
+                print_usage "Unknown flag argument: $arg"
+                exit 1
+                ;;
+            *)
+                userargs=("${userargs[@]}" "$arg")
+        esac
+    done
+
+    # Script was executed.
+    # Automatically disable colors if stdout is not a tty, unless forced.
+    ((do_forced)) || colr_auto_disable 1
+    colr "${userargs[@]}"
+fi
---
+++ test_colr.sh
@@ -0,0 +1,232 @@
+#!/bin/bash
+
+# Just print some color combos out.
+# -Christopher Welborn 09-25-2015
+appname="test_colr"
+appversion="0.0.2"
+apppath="$(readlink -f "${BASH_SOURCE[0]}")"
+appscript="${apppath##*/}"
+appdir="${apppath%/*}"
+
+source "$appdir/colr.sh"
+
+function echo_err {
+    # Echo to stderr.
+    echo -e "$@" 1>&2
+}
+
+function fail {
+    # Echo to stderr and exit with an error status code.
+    echo_err "$@"
+    exit 1
+}
+
+function get_max_cols {
+    # Get maximum number of columns to show for each row,
+    # based on number of items available and terminal width.
+    # Arguments:
+    #   $1 : Overall item count (all columns).
+    #   $2 : Width of each item (col width).
+    local itemcount=$1
+    [[ -z "$1" ]] && fail "Expected item count for get_max_cols!"
+    local colwidth=$2
+    [[ -z "$2" ]] && fail "Expected column width for get_max_cols!"
+    # echo_err "ITEM COUNT: $itemcount"
+    # echo_err " COL WIDTH: $colwidth"
+    # Get terminal width, default to 80 on tput error.
+    local termcols
+    termcols="$(tput cols)" || termcols=80
+    [[ -z "$termcols" ]] && termcols=80
+    # echo_err "TERM WIDTH: $termcols"
+    # Maximum number of columns that will fit.
+    local colmaxwidth=$((termcols / colwidth - 1))
+    # echo_err "  MAX COLS: $colmaxwidth"
+    # Maximum number of columns to use for each row, default is the max.
+    local columncnt=$colmaxwidth
+    # Determine most columns to use for each row, looking for 'even' fit.
+    local trycolumncnt
+    for trycolumncnt in $(seq "$colmaxwidth" -1 3); do
+        # echo_err "Trying $itemcount & $trycolumncnt == $((itemcount % trycolumncnt))"
+        if ((itemcount % trycolumncnt == 0)); then
+            columncnt=$trycolumncnt
+            break
+        fi
+    done
+    printf "%s" "$columncnt"
+}
+
+function pad_text {
+    # Pads the F: color, B: color labels.
+    # Arguments:
+    #   $1 : Label to pad.
+    #   $2 : Width to pad. Default: 35
+    local width=$2
+    [[ -z "$width" ]] && fail "Width expected for pad_text!"
+    printf "%-*s" "$width" "$1"
+}
+
+function pad_num {
+    # Pad a number with 0's.
+    # Arguments:
+    #   $1 : Number to pad.
+    #   $2 : Width to pad. Default: 3
+    local width="${2:-3}"
+    printf "%-*s" "$width" "$1"
+}
+
+function print_named {
+    # Print all named colors.
+    # Max width of each label/column.
+    local colwidth=30
+    # Max number of columns per row.
+    local columncnt
+    # Get a count of named fore color keys.
+    local fore_total=0
+    local numpat='^[0-9]'
+    local forename
+    # shellcheck disable=SC2154
+    # ..fore is sourced.
+    for forename in "${!fore[@]}"; do
+        [[ "$forename" =~ $numpat ]] && continue
+        let fore_total+=1
+    done
+    columncnt="$(get_max_cols "$fore_total" "$colwidth")"
+    local cnt=1
+    local forecnt=0
+    local backname
+    local label_text
+    # shellcheck disable=SC2154
+    # ..fore and back are sourced.
+    for backname in "${!back[@]}"; do
+        # Skip number keys, use names only.
+        [[ "$backname" =~ $numpat ]] && continue
+        for forename in "${!fore[@]}"; do
+            [[ "$forename" =~ $numpat ]] && continue
+            let forecnt+=1
+            label_text="B:$(pad_text "$backname" 11), F:$(pad_text "$forename" 11)"
+            printf "%-40s" "$(colr "$(pad_text "$label_text" "$colwidth")" "$forename" "$backname")"
+            if (( cnt == columncnt )); then
+                printf "\n"
+                let cnt=1
+            else
+                let cnt+=1
+                ((forecnt < fore_total)) && printf "|"
+            fi
+        done
+        let forecnt=0
+        let cnt=1
+        printf "\n"
+    done
+}
+
+function print_ranges {
+    # Print a range of numbered colors.
+    # Arguments:
+    #   $1 : Fore start.
+    #   $2 : Fore end.
+    #   $3 : Back start.
+    #   $4 : Back end.
+    local fore_start="${1:-0}"
+    local fore_end="${2:-255}"
+    local back_start="${3:-0}"
+    local back_end="${4:-255}"
+    # Clamp values at 0-255.
+    ((fore_start < 0)) && fore_start=0
+    ((fore_end > 255)) && fore_end=255
+    ((back_start < 0)) && back_start=0
+    ((back_end > 255)) && back_end=255
+    # Maximum column width with label and numbers.
+    local colwidth=13
+    # Number of fore colors for each back color.
+    local fore_total=$((fore_end - fore_start + 1))
+    local columncnt
+    columncnt="$(get_max_cols "$fore_total" "$colwidth")"
+
+    local cnt=1
+    local forecnt=0
+    local backnum
+    local forenum
+    local label_text
+    for backnum in $(seq "$back_start" "$back_end"); do
+        for forenum in $(seq "$fore_start" "$fore_end"); do
+            let forecnt+=1
+            label_text="B:$(pad_num "$backnum"), F:$(pad_num "$forenum")"
+            colr "$(pad_text "$label_text" "$colwidth")" "$forenum" "$backnum"
+            if (( cnt == columncnt )); then
+                printf "\n"
+                let cnt=1
+            else
+                let cnt+=1
+                ((forecnt < fore_total)) && printf "|"
+            fi
+        done
+        let forecnt=0
+        let cnt=1
+        printf "\n"
+    done
+}
+
+function print_usage {
+    # Show usage reason if first arg is available.
+    [[ -n "$1" ]] && echo -e "\n$1\n"
+
+    echo "$appname v. $appversion
+
+    Usage:
+        $appscript -h | -v
+        $appscript [-2 | -n]
+        $appscript FORE_START [FORE_END] [BACK_START] [BACK_END]
+
+    Options:
+        FORE_START    : Starting number for fore color range.
+        FORE_END      : Ending number for fore color range.
+                        Default: 255
+        BACK_START    : Starting number for back color range.
+                        Default: 0
+        BACK_END      : Ending number for back color range.
+                        Default: 255
+        -h,--help     : Show this message.
+        -2,--256      : Use 0-255 instead of names.
+        -n,--names    : Use all named colors. This is the default.
+        -v,--version  : Show $appname version and exit.
+    "
+}
+
+declare -a ranges
+do_256=0
+
+for arg
+do
+    case "$arg" in
+        "-2"|"--256" )
+            do_256=1
+            ;;
+        "-h"|"--help" )
+            print_usage ""
+            exit 0
+            ;;
+        "-n"|"--names" )
+            do_256=0
+            ;;
+        "-v"|"--version" )
+            echo -e "$appname v. $appversion\n"
+            exit 0
+            ;;
+        -*)
+            print_usage "Unknown flag argument: $arg"
+            exit 1
+            ;;
+        *)
+            ranges=("${ranges[@]}" "$arg")
+    esac
+done
+
+if ((${#ranges[@]} > 0)); then
+    # User has passed some range args.
+    print_ranges "${ranges[0]:-0}" "${ranges[1]:-255}" "${ranges[2]:-0}" "${ranges[3]:-255}"
+elif ((do_256)); then
+    print_ranges
+else
+    print_named
+fi
+exit
